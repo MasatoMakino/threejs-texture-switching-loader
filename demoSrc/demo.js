647f17b4642829c389bb1213e704d65a0cdac329
MasatoMakino <unagiinu.g@gmail.com>
2020-07-18 09:49:21 +0900

    change : demo script.
    Cache有功時の挙動確認のため。

--- demoSrc/demo.js
+++ demoSrc/demo.js
@@ -6,9 +6,9 @@ import {
   initControl,
   initRenderer,
   initHelper,
-  render
+  render,
 } from "./common";
-import { Mesh, MeshBasicMaterial, SphereGeometry } from "three";
+import { Mesh, MeshBasicMaterial, SphereGeometry, Cache } from "three";
 
 const W = 640;
 const H = 480;
@@ -24,29 +24,50 @@ const onDomContentsLoaded = () => {
   render(control, renderer, scene, camera);
 };
 
-const initSphere = scene => {
+const initSphere = (scene) => {
   const geo = new SphereGeometry(20, 16, 16);
   const mat = new MeshBasicMaterial();
   const mesh = new Mesh(geo, mat);
   scene.add(mesh);
 
+  let texture1;
+  let texture2;
+
+  Cache.enabled = true;
   const loader = new TextureSwitchingLoader();
   loader
     .load("./earth.jpg", { imageBitmapOption: { imageOrientation: "flipY" } })
-    .then(texture => {
+    .then((texture) => {
       mat.map = texture;
       mat.needsUpdate = true;
-      console.log(texture);
+      console.log("1st Load");
+      console.log(texture.image);
+      texture1 = texture;
+
+      loader.load("./earth.jpg").then((texture) => {
+        console.log("2nd Load");
+        console.log(texture.image);
+        texture2 = texture;
+
+        if (texture1.image === texture2.image) {
+          console.log("share!");
+        }
+      });
     });
-  loader.load("./earth.jpg").then(texture => {
-    console.log(texture);
-  });
 
   loader.load("./earth.jpg");
   loader.load("./earth.jpg");
   loader.load("./earth.jpg");
   loader.load("./earth.jpg");
   loader.load("./earth.jpg");
+
+  setTimeout(() => {
+    loader.load("./earth.jpg");
+    loader.load("./earth.jpg");
+    loader.load("./earth.jpg");
+    loader.load("./earth.jpg");
+    loader.load("./earth.jpg");
+  }, 2000);
 };
 
 window.onload = onDomContentsLoaded;
