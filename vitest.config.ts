f195777a95a9a4ef29c902f78ad92e81ae679a1c
MasatoMakino <makino.masato.g@gmail.com>
2024-08-04 15:12:54 +0900

    migrate : node-canvas to vitest/browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,21 +2,16 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    name: "jsdomTest",
-    environment: "jsdom",
-    environmentOptions: {
-      jsdom: {
-        resources: "usable",
-      },
+    browser: {
+      enabled: true,
+      name: "chrome",
+      headless: true,
+      provider: "webdriverio",
     },
     coverage: {
       provider: "istanbul",
       reporter: ["text", "html", "json", "lcov"],
       include: ["src/**/*"],
     },
-  },
-  server: {
-    host: true,
-    port: 3000,
   },
 });
